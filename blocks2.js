a33dd1e6984859910882ae35a14ab3866b9f15ec
Sap-teach <71496687+Sap-teach@users.noreply.github.com>
2020-11-13 23:35:17 +0530

    Add files via upload

---
+++ blocks2.js
@@ -0,0 +1,27 @@
+class blocks2{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.4,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        strokeWeight(0);
+        fill(4, 49, 151);
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
+  }
